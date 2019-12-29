10c960aaf6847d75d55c07c782d6751ebe8ad754
anamolsoman <anamol@astutertech.com>
2019-12-29 17:33:25 +0530

    Vuejs image gallery

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
