6563f91abb04d364a2a9a13b117d1258246255fb
anamolsoman <anamol@astutertech.com>
2019-12-31 11:53:11 +0530

    code fixed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import Gallery from "~/components/Gallery.vue";
 
 Vue.use(Vuetify);
 
@@ -24,6 +25,6 @@ export default {
   install(Vue) {
     // Let's register our component globally
     // https://vuejs.org/v2/guide/components-registration.html
-    Vue.component("Gallery", require("@/components/Gallery.vue").default);
+    Vue.component("Gallery", Gallery);
   }
 };
